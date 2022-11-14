ed6e8694d7cb952c794afbc56c6e1d7147f67f65
Aftab69 <aftabhssn3@gmail.com>
2022-11-15 02:56:09 +0530

    viewports added in projectsmobile page

--- src/Projectsmobile.js
+++ src/Projectsmobile.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import "./Projectsmobile.css"
 import ReactPlayer from "react-player"
