114bf1f5bbc4153168d10c5a807c16247d8af28c
Aftab69 <aftabhssn3@gmail.com>
2022-11-14 16:38:34 +0530

    target blanks removed and netlify toml added

--- src/Projects.js
+++ src/Projects.js
@@ -19,7 +19,7 @@ const Projects = (props) => {
                   <Zoom><ReactPlayer className='eachVideo' url='https://www.youtube.com/watch?v=-bpQpsO7mWs' controls width="720px" height="480px" /></Zoom>
                   <Zoom><ReactPlayer className='eachVideo' url='https://www.youtube.com/watch?v=POuwAy2JTLk' controls width="720px" height="480px" /></Zoom>
                 </div>
-                <Link style={{textDecoration:"none"}} to="/musicvideos" target="_blank"><button name="button1" id="button1" style={{display:"block"}}>View more</button></Link>
+                <Link style={{textDecoration:"none"}} to="/musicvideos"><button name="button1" id="button1" style={{display:"block"}}>View more</button></Link>
             </div>
             <div className='videoCategoryHeading'><p>Ad Films :</p></div>
             <div className='videosOuterContainer'>
@@ -27,14 +27,14 @@ const Projects = (props) => {
                 <Zoom><ReactPlayer className='eachVideo' url='https://www.youtube.com/watch?v=qWxFXk5CWJQ' controls width="720px" height="480px" /></Zoom>
                 <Zoom><ReactPlayer className='eachVideo' url='https://www.youtube.com/watch?v=BBItkycabOM' controls width="720px" height="480px" /></Zoom>
               </div>
-              <Link style={{textDecoration:"none"}} to="/adfilms" target="_blank"><button name="button2" id="button2">View more</button></Link>
+              <Link style={{textDecoration:"none"}} to="/adfilms"><button name="button2" id="button2">View more</button></Link>
             </div>
             <div className='videoCategoryHeading'><p>Fashion Films :</p></div>
             <div className='videosOuterContainer'>
               <div className='videosInnerContainer' id='videosofbutton3'>
                 <Zoom><ReactPlayer className='eachVideo' url='https://www.youtube.com/watch?v=s2xr1_Pr_6U' controls width="720px" height="480px" /></Zoom>
               </div>
-              <Link style={{textDecoration:"none"}} to="/fashionfilms" target="_blank"><button name="button3" id="button3">View more</button></Link>
+              <Link style={{textDecoration:"none"}} to="/fashionfilms"><button name="button3" id="button3">View more</button></Link>
             </div>
             <div className='videoCategoryHeading'><p>Short/Documentary/Travel Films :</p></div>
             <div className='videosOuterContainer'>
@@ -42,7 +42,7 @@ const Projects = (props) => {
                 <Zoom><ReactPlayer className='eachVideo' url='https://www.youtube.com/watch?v=IASy9tKBzlw' controls width="720px" height="480px" /></Zoom>
                 <Zoom><ReactPlayer className='eachVideo' url='https://www.youtube.com/watch?v=_yQ1rMLRpGk' controls width="720px" height="480px" /></Zoom>
               </div>
-              <Link style={{textDecoration:"none"}} to="/shortdocumentarytravelfilms" target="_blank"><button name="button4" id="button4">View more</button></Link>
+              <Link style={{textDecoration:"none"}} to="/shortdocumentarytravelfilms"><button name="button4" id="button4">View more</button></Link>
             </div>
             <div className='videoCategoryHeading'><p>Behind the Scenes :</p></div>
             <div className='videosOuterContainer'>
@@ -50,7 +50,7 @@ const Projects = (props) => {
                 <Zoom><ReactPlayer className='eachVideo' url='https://www.youtube.com/watch?v=0bqRgquI-0g' controls width="720px" height="480px" /></Zoom>
                 <Zoom><ReactPlayer className='eachVideo' url='https://www.youtube.com/watch?v=H2UnUAGWGak' controls width="720px" height="480px" /></Zoom>
               </div>   
-              <Link style={{textDecoration:"none"}} to="/behindthescenes" target="_blank"><button name="button5" id="button5">View more</button></Link>
+              <Link style={{textDecoration:"none"}} to="/behindthescenes"><button name="button5" id="button5">View more</button></Link>
             </div>      
         </div>
       </div>
