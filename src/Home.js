d3db8666a863edc1683a839219752c34720ed1cd
Aftab69 <aftabhssn3@gmail.com>
2022-11-15 02:14:43 +0530

    loading screen check

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "./Home.css"
 import useMediaQuery from "./useMediaQuery"
 // import showreel from "./Images/Showreel.mp4"
